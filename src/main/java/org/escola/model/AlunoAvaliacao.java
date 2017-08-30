cd878bebf5eba9146d63b9e477ead5b29df3c9fa
a131mael <a131mael@gmail.com>
2017-08-30 15:13:41 -0300

    a

---
+++ src/main/java/org/escola/model/AlunoAvaliacao.java
@@ -0,0 +1,98 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.escola.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "id") )
+public class AlunoAvaliacao implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "GENERATE_aluno_avaliacao", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "GENERATE_aluno_avaliacao", sequenceName = "Aluno_avaliacao_pk_seq", allocationSize = 1)
+	private Long id;
+
+	@ManyToOne
+	private Aluno aluno;
+	
+	@ManyToOne
+	private Avaliacao avaliacao;
+
+	@Column
+	private float nota;
+	
+	@Column
+    private int anoLetivo;
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public Avaliacao getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(Avaliacao avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public float getNota() {
+		return nota;
+	}
+
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getAnoLetivo() {
+		return anoLetivo;
+	}
+
+	public void setAnoLetivo(int anoLetivo) {
+		this.anoLetivo = anoLetivo;
+	}
+}
---
+++ src/main/java/org/escola/model/MatchStatusEnum.java
@@ -0,0 +1,21 @@
+package org.escola.model;
+
+public enum MatchStatusEnum {
+
+	PLAYED("Played"),
+	
+	PLAYING("PLAYING"),
+	
+	NOT_PLAYED("Not played");
+	
+	private String label;
+    
+	
+	MatchStatusEnum(String name){
+		this.label = name;
+	}
+	
+    public String getLabel() {
+        return label;
+    }
+}
